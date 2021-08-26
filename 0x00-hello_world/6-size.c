995ffa83eb3c3ecf365dada0a8d366e4df69438b
Kibagendi <maingawesley@gmail.com>
2021-08-26 12:53:23 -0700

    intel syntax

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 * main - print the string in the put function
 *
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
+*this program prints the size of various types on the computer it is compiled and run on.
 * Return: 0
 */
 int main(void)
