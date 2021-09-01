05c34f6d681b456fd8974e0e9cb171fd83c3e9fb
Kibagendi <maingawesley@gmail.com>
2021-09-01 10:59:18 -0700

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ *print_alphabet - prints alphabet in lowercase
+ *
+ *Return: always 0.
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+		
+	}
+
+_putchar('\n');	
+}
