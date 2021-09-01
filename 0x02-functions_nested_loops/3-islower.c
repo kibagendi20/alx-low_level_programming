bbab5db1c98a774fdfc9a0a3c1f754596f83936c
Kibagendi <maingawesley@gmail.com>
2021-09-01 11:08:40 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include<ctype.h>
+#include"main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	islower(d);
+}
+int _islower(int c)
+{
+	char c;
+
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
