e40df8a9cf2f31975726e86909765b13aab04ac7
Kibagendi <maingawesley@gmail.com>
2021-09-06 13:06:08 -0700

    returning string length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,17 @@
 #include "main.h"
-#include <string.h>
 /**
  *_strlen - return the length of the string variable passed
  *@s: pointer to the string variable
  */
 int _strlen(char *s)
 {
-	strlen(*s);
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		len++
+	}
+
+	return (len);
 }
