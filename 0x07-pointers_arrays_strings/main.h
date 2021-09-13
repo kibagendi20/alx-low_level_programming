9c5c7c65781dabf011d01514befd4e4616f5fa57
Kibagendi <maingawesley@gmail.com>
2021-09-13 12:46:57 -0700

    function to locate substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,6 +10,7 @@ int _putchar.c
 void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 #endif
