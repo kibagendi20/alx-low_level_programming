f3a49521763eeb7df9362ff8d9ce7c75707d03d7
Kibagendi <maingawesley@gmail.com>
2021-09-13 12:28:38 -0700

    printing chessboard

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ int _putchar.c
 void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 #endif
