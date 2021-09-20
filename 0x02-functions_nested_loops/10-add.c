811bcfa985104eb684182cc60df1dcf2d054d01c
Kibagendi <maingawesley@gmail.com>
2021-09-19 22:39:21 -0700

    defined add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: number to be summed with b
+ * @b: added to a
+ * Description: Adds the two operands and stores the answer to sum var
+ * Return: summation of the operands
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
