7dcf2cfee872f27662e0a099a44290445dba0d9f
Kibagendi <maingawesley@gmail.com>
2021-09-20 02:18:48 -0700

    updated with _abs() prototype

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,6 +8,7 @@ void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
+int _abs(int);
 int add(int, int);
 int print_last_digit(int);
 void print_to_98(int n);
