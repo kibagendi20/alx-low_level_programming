ff0dd37f2cf1335ac8852283a7bfd31f78482039
Kibagendi <maingawesley@gmail.com>
2021-09-20 02:18:06 -0700

    _abs function definition

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *_abs - computes absolute value of an integer
+ *@n: argument to be passed
+ *Return: 1
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n *= -1;
+
+	return (n);
+}
